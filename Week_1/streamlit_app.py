5b17e97c61fac01d6003c012b039c2196e8f942a
Azmi Yağlı <52624316+yagli18@users.noreply.github.com>
2023-03-19 10:38:58 +0300

    Update streamlit_app.py

--- Week_1/streamlit_app.py
+++ Week_1/streamlit_app.py
@@ -20,8 +20,8 @@ dataframe = pd.read_csv(
 )
 
 # We have a limited budget, therefore we would like to exclude
-# listings with a price above 2000 turkish lira per night
-dataframe = dataframe[dataframe["Price"] <= 2000]
+# listings with a price above 1000 turkish lira per night
+dataframe = dataframe[dataframe["Price"] <= 1000]
 
 # Display as integer
 dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
